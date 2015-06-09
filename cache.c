aa3165f8cb1e1e7f4db3b14360713866388e0923
Lucas Soumille <soumille.lucas@etu.unice.fr>
2015-06-09 10:35:28 +0200

    Cache_Create

--- cache.c
+++ cache.c
@@ -1,3 +1,4 @@
+
 /*!
  * \file low_cache.c
  * 
@@ -5,6 +6,7 @@
  */
 
 #include "low_cache.h"
+#include "cache.c"
 
 //! Retourne le premier bloc libre ou NULL si le cache est plein
 struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache)
@@ -14,4 +16,25 @@ struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache)
 	pcache->pfree = *(pcache->pfree)+1;
 	
 	return libre;
+}
+
+//! Création du cache.
+struct Cache *Cache_Create(const char *fic, unsigned nblocks, unsigned nrecords,
+                           size_t recordsz, unsigned nderef){
+	struct Cache * cache = malloc(sizeof(struct Cache));
+	cache->file = fic;
+	cache->fp = fopen(fic, "r+");
+	cache->nblocks = nblocks;
+	cache->nderef = nderef;
+	cache->blocksz = recordsz * nrecords;
+	cache->nrecords = nrecords;
+	cache->recordsz = recordsz;
+	cache->pstrategy = NULL;
+	struct Cache_Instrument instr = malloc(sizeof(struct Cache_Instrument));
+	cache->instrument = instr;
+	struct Cache_Block_Header * headers = malloc(sizeof(struct Cache_Block_Header) * nblocks);
+	cache->headers = headers;
+	cache->pfree = Get_Free_Block(cache);
+
+	return cache;
 }
