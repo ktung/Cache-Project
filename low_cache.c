553c0481949c3f8f5774abe0029881fe2bb74869
ThibautSoriano <st405262@etu.unice.fr>
2015-06-09 11:17:43 +0200

    created low_cache.c

---
+++ low_cache.c
@@ -0,0 +1,14 @@
+/*!
+ * \file low_cache.c
+ * 
+ * \author Thibaut SORIANO Aina Rapenomanana
+ */
+
+#include "low_cache.h"
+
+
+struct Cache_Block_Header *Get_Free_Block(struct Cache *pcache)
+{
+	
+	return NULL;
+}
