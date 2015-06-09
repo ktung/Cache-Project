4fc197ff9ae8c90728075704dc6a789a2d2dca4a
Natiora Rapenomanana <rn406826@etu.unice.fr>
2015-06-09 15:06:23 +0200

    méthode Append, Clear, Empty

--- cache_list.c
+++ cache_list.c
@@ -19,7 +19,21 @@ struct Cache_List *Cache_List_Create()
 void Cache_List_Delete(struct Cache_List *list);
 
 /*! Insertion d'un élément à la fin */
-void Cache_List_Append(struct Cache_List *list, struct Cache_Block_Header *pbh);
+void Cache_List_Append(struct Cache_List *list, struct Cache_Block_Header *pbh) {
+	// pbh -> créer une Cache list
+	struct Cache_List cache_list = Cache_List_Create();
+	cache_list->pheader = pbh;
+
+	// Parcourir la list jusqu'a trouver next == null
+	struct Cache_List *tmp = list;
+	for(; tmp->next != NULL; tmp=tmp->next);
+
+	// Avec le dernier bloc, le next du dernier sera le nouveau
+	tmp->next = cache_list;
+	// le prev du nouveau sera l'ancien dernier
+	cache_list->prev = tmp;
+
+}
 /*! Insertion d'un élément au début*/
 void Cache_List_Prepend(struct Cache_List *list, struct Cache_Block_Header *pbh);
 
@@ -32,10 +46,23 @@ struct Cache_Block_Header *Cache_List_Remove(struct Cache_List *list,
                                              struct Cache_Block_Header *pbh);
 
 /*! Remise en l'état de liste vide */
-void Cache_List_Clear(struct Cache_List *list);
+void Cache_List_Clear(struct Cache_List *list) {
+	struct Cache_List *tmp = list;
+	// Parcourir la list jusqu'a trouver next == null (on arrive à la fin de la liste)
+	for(; tmp->next != NULL; tmp=tmp->next) {
+		tmp->pheader = NULL;
+	}
+
+}
 
 /*! Test de liste vide */
-bool Cache_List_Is_Empty(struct Cache_List *list);
+bool Cache_List_Is_Empty(struct Cache_List *list) {
+	bool res = true;
+	for(; tmp->next != NULL; tmp=tmp->next) {
+		if(tmp->pheader == NULL) res &= true;
+	}
+	return res;
+}
 
 /*! Transférer un élément à la fin */
 void Cache_List_Move_To_End(struct Cache_List *list,
