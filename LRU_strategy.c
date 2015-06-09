a0c409784ee04328ae7d91df7b87ac78946425a6
Natiora Rapenomanana <rn406826@etu.unice.fr>
2015-06-09 13:20:46 +0200

    pb cache.c

--- LRU_strategy.c
+++ LRU_strategy.c
@@ -1,9 +1,10 @@
 /*!
  * \file LRU_strategy.c
  *
- * \brief  Stratégie de remplacement avec LRU..
+ * \brief  Stratégie de remplacement avec LRU.
  * 
- * \author Jean-Paul Rigault
+ * \author Tùtùre Rapenomanana
+ * \author Thibaut Soriano
  *
  * $Id: LRU_strategy.c,v 1.3 2008/03/04 16:52:49 jpr Exp $
  */
@@ -13,11 +14,7 @@
 #include "cache_list.h"
 
 /*!
- * LRU 
- *
- * En fait, nous initialisons le germe
- * (seed) du générateur aléatoire à quelque chose d'éminemment variable, pour
- * éviter d'avoir la même séquence à chque exécution...
+ * LRU : Creation de la Cache_List 
  */
 void *Strategy_Create(struct Cache *pcache) 
 {
@@ -25,15 +22,15 @@ void *Strategy_Create(struct Cache *pcache)
 }
 
 /*!
- * LRU : Rien à faire ici.
+ * LRU : Suppression de la Cache List 
  */
 void Strategy_Close(struct Cache *pcache)
 {
-	Cache_List_Delete(pcache->pstrategy);
+    Cache_List_Delete(pcache->pstrategy);
 }
 
 /*!
- * LRU : Rien à faire ici.
+ * FIFO : Vide la Cache List.
  */
 void Strategy_Invalidate(struct Cache *pcache)
 {
@@ -41,7 +38,7 @@ void Strategy_Invalidate(struct Cache *pcache)
 }
 
 /*! 
- * LRU : On prend le premier bloc invalide. S'il n'y en a plus, on prend un bloc au hasard.
+ * LRU : Retourne un block free disponible ou supprime le 1er de la Cache_List.
  */
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
 {
@@ -59,23 +56,19 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache)
 
 
 /*!
- * LRU : Rien à faire ici.
+ * LRU : Stratégie seulement sur la date de création.
  */
 void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
 {
-	struct Cache_List *tmpList = (struct Cache_List*)pcache->pstrategy;
-	struct Cache_Block_Header *header = Cache_List_Remove(tmpList, pbh);
-	Cache_List_Append(tmpList, header);
+    Cache_List_Move_To_End((struct Cache_List*)pcache->pstrategy, pbh);
 }  
 
 /*!
- * LRU : Rien à faire ici.
+ * LRU : Stratégie seulement sur la date de création.
  */  
 void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
 {
-	struct Cache_List *tmpList = (struct Cache_List*)pcache->pstrategy;
-	struct Cache_Block_Header *header = Cache_List_Remove(tmpList, pbh);
-	Cache_List_Append(tmpList, header);
+    Cache_List_Move_To_End((struct Cache_List*)pcache->pstrategy, pbh);
 } 
 
 char *Strategy_Name()
